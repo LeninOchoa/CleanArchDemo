99889d76fc03cdc129ac5767ed0ab09bc88bebe4
Lenin Ochoa <isleor@hotmail.com>
2021-07-01 18:21:56 +0200

    Adding Domain.Core and Implementation of InMemoryBus

---
+++ CleanArch/CleamArch.Domain.Core/Bus/IMediatorHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using CleamArch.Domain.Core.Commands;
+
+namespace CleamArch.Domain.Core.Bus
+{
+    public interface IMediatorHandler
+    {
+        Task SendCommand<T>(T command) where T : Command;
+    }
+}
---
+++ CleanArch/CleamArch.Domain.Core/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CleamArch.Domain.Core.Events;
+
+namespace CleamArch.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
